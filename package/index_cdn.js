23905d85caf376aa262d672495ad873f9eaf23a1
JasperCherry <kacper.wisnicki@gmail.com>
2022-10-05 20:38:21 +0100

    update

--- package/index_cdn.js
+++ package/index_cdn.js
@@ -1,75 +1,78 @@
-const joro = {};
-joro.spiderName = '';
-joro.scrollThrottle = 500;
-joro.presenceThrottle = 1000;
-joro.presenceTimeout = 60;
-joro.scrollWait = false;
-joro.stopSpider = false;
-joro.streamData = function(data) { };
+function createJoro(config) {
+  const joro = {};
+  joro.spiderName = config?.spiderName || '';
+  joro.scrollThrottle = config?.scrollThrottle || 500;
+  joro.presenceThrottle = config?.presenceThrottle || 1000;
+  joro.presenceTimeout = config?.presenceTimeout || 60;
+  joro.scrollWait = false;
+  joro.startSpider = false;
+  joro.stopSpider = false;
+  joro.streamData = function(data) { };
 
-joro.createCommonData = function(type) {
-  const data = {};
-  data.type = type;
-  data.timestamp = Date.now();
-  data.url = window.location.href;
-  data.windowWidth = window.innerWidth;
-  data.windowHeight = window.innerHeight;
-  data.userId = joro.userId;
-  data.spiderName = joro.spiderName;
+  joro.createCommonData = function(type) {
+    const data = {};
+    data.type = type;
+    data.timestamp = Date.now();
+    data.url = window.location.href;
+    data.windowWidth = window.innerWidth;
+    data.windowHeight = window.innerHeight;
+    data.userId = joro.userId;
+    data.spiderName = joro.spiderName;
 
-  return data;
-}
-
-joro.delayPresence = function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
-}
-
-joro.start = async function(config) {
-  joro.spiderName = config?.spiderName || joro.spiderName;
-  joro.scrollThrottle = config?.scrollThrottle || joro.scrollThrottle;
-  joro.presenceThrottle = config?.presenceThrottle || joro.presenceThrottle;
-  joro.presenceTimeout = config?.presenceTimeout || joro.presenceTimeout;
-
-  let joroUserId = localStorage.getItem('joroUserId');
-  if (!joroUserId) {
-    joroUserId = crypto.randomUUID();
-    localStorage.setItem('joroUserId', joroUserId);
+    return data;
   }
-  joro.userId = joroUserId;
 
-  document.addEventListener('scroll', function(event){
-    if (joro.stopSpider) return;
+  joro.delayPresence = function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+  }
 
-    if (!joro.scrollWait) {
-      joro.scrollWait = true;
-      setTimeout(function () {
-          joro.scrollWait = false;
-          const data = joro.createCommonData('scroll');
-          data.scrollX = window.scrollX;
-          data.scrollY = window.scrollY;
-          joro.streamData(data);
-      }, joro.scrollThrottle);
+  joro.start = async function() {
+    if (joro.startSpider) return;
+    joro.startSpider = true;
+
+    let joroUserId = localStorage.getItem('joroUserId');
+    if (!joroUserId) {
+      joroUserId = crypto.randomUUID();
+      localStorage.setItem('joroUserId', joroUserId);
     }
-  });
+    joro.userId = joroUserId;
 
-  document.addEventListener('click', function(event){
-    if (joro.stopSpider) return;
+    document.addEventListener('scroll', function(event){
+      if (joro.stopSpider) return;
 
-    const data = joro.createCommonData('click');
-    data.mouseX = event.pageX;
-    data.mouseY = event.pageY;
-    joro.streamData(data);
-  });
+      if (!joro.scrollWait) {
+        joro.scrollWait = true;
+        setTimeout(function () {
+            joro.scrollWait = false;
+            const data = joro.createCommonData('scroll');
+            data.scrollX = window.scrollX;
+            data.scrollY = window.scrollY;
+            joro.streamData(data);
+        }, joro.scrollThrottle);
+      }
+    });
 
-  for (let i = 0; i < joro.presenceTimeout; i += 1) {
-    if (joro.stopSpider) return;
+    document.addEventListener('click', function(event){
+      if (joro.stopSpider) return;
 
-    const data = joro.createCommonData('presence');
-    joro.streamData(data);
-    await joro.delayPresence(joro.presenceThrottle);
+      const data = joro.createCommonData('click');
+      data.mouseX = event.pageX;
+      data.mouseY = event.pageY;
+      joro.streamData(data);
+    });
+
+    for (let i = 0; i < joro.presenceTimeout; i += 1) {
+      if (joro.stopSpider) return;
+
+      const data = joro.createCommonData('presence');
+      joro.streamData(data);
+      await joro.delayPresence(joro.presenceThrottle);
+    }
+  };
+
+  joro.stop = function() {
+    joro.stopSpider = true;
   }
-};
 
-joro.stop = function() {
-  joro.stopSpider = true;
+  return joro;
 }
